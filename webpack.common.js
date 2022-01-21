14436d99c7d7d104379ba10d49e4d8d88db5f22b
Narmin Aziz <narmin.aziz23@gmail.com>
2022-01-21 09:15:33 +0400

    fixing bug

--- webpack.common.js
+++ webpack.common.js
@@ -8,19 +8,11 @@ module.exports = {
   entry: {
     index: './src/js/index.js'
   },
-  // output: {
-  //   filename: './js/main.js',
-  //   path: path.resolve(__dirname, 'dist'),
-  //   clean: true,
-  // },
   plugins: [
     new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
-    // new MiniCssExtractPlugin({
-    //   filename: "./css/main.css"
-    // }),
     new HtmlWebpackPlugin({
       title: 'Chart System',
-      filename: '/index.html',
+      filename: 'index.html',
       template: 'src/template.html'
     }),
   ],
--- webpack.prod.js
+++ webpack.prod.js
@@ -5,18 +5,16 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = merge(common, {
   mode: 'production',
-  entry: {
-    index: './src/js/index.js'
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: "./css/main.[contenthash].css"
-    }),
-  ],
   output: {
     filename: './js/main.[contenthash].js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
     publicPath: '',
   },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "./css/main.[contenthash].css"
+    }),
+  ],
+  
 })
